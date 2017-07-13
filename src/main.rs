5d183d7068cf81501a40818e0c4127d573344b9c
JohnBCasey <caseyjohnb@gmail.com>
2017-07-12 22:51:42 -0400

    accepting connections and printing http request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::io::*;
+use std::net::{TcpListener, TcpStream};
+
+
+
+pub fn listen_to_msg(){
+
+	let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+	// accept connections and process them serially
+	for stream in listener.incoming() {
+	    match stream {
+	        Ok(mut stream) => {
+	        	println!("connected!");
+	        	let mut buffer = [0; 512];
+	        	let buff = stream.read(&mut buffer).unwrap();
+	        	println!("{}",  std::str::from_utf8(&buffer).unwrap());
+	        }
+	        Err(e) => { /* connection failed */ }
+	    }
+	}
+
+}
+
+
+
+
+
+
+
+
 fn main() {
     println!("Hello, world!");
+    listen_to_msg()
 }
