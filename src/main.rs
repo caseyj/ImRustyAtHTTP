9410c854614fdf836ed751d4e3dbbe84e4da6bb6
JohnBCasey <caseyjohnb@gmail.com>
2017-07-12 23:30:51 -0400

    now writes an HTML doc to screen

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
-use std::io::*;
+use std::io;
+use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use std::fs::File;
+
+
+pub fn respond(mut stream : TcpStream){
+	let mut serve = String::new();
+	let mut f = File::open("index.html").unwrap();
+	f.read_to_string(&mut serve).unwrap();
+	let mut writer : String = "HTTP/1.0 200 OK\nContent-type: text/html\n\n\n".to_owned();
+
+	writer.push_str(&serve);
+	println!("response: {}", writer);
+	stream.write(writer.as_bytes());
+}
 
 
 
@@ -15,8 +29,10 @@ pub fn listen_to_msg(){
 	        	let mut buffer = [0; 512];
 	        	let buff = stream.read(&mut buffer).unwrap();
 	        	println!("{}",  std::str::from_utf8(&buffer).unwrap());
+	        	respond(stream);
+	        	
 	        }
-	        Err(e) => { /* connection failed */ }
+	        Err(e) => { println!("Failure") }
 	    }
 	}
 
