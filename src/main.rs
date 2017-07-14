1ed6d1cb23969a807a84dda8031406f402f3d7c9
JohnBCasey <caseyjohnb@gmail.com>
2017-07-13 21:10:26 -0400

    cleaned up files a bit and added some comments

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 use std::fs::File;
 
+
+
+
 pub struct HTTP_REQ{
 	pub method: String,
 	pub file: String,
@@ -19,10 +22,15 @@ impl HTTP_REQ{
 	}
 }
 
-
+/**
+*Responds to the HTTP request sent with either a document or 404 error if document not found
+*Will be expanded for POST
+*
+*
+*
+*/
 pub fn respond(mut stream : TcpStream, req: HTTP_REQ){
 	let mut serve = String::new();
-	println!("{}", req.file);
 	let mut writer : String;
 	match File::open(req.file){
 		Ok(mut f)=>{
@@ -31,18 +39,21 @@ pub fn respond(mut stream : TcpStream, req: HTTP_REQ){
 				writer.push_str(&serve);
 			},
 		Err(e)=>{
-			writer = "HTTP/1.0 400 OK\nContent-type: text/html\n\n\n".to_owned()
+			//I assume 404 for now
+			writer = "HTTP/1.0 404 OK\nContent-type: text/html\n\n\n".to_owned()
 		}
 	}
-	
-	println!("response: {}", writer);
 	stream.write(writer.as_bytes());
 }
 
 
 
-
-pub fn listen_to_msg(){
+/**
+*Runs the server, opensa tcp channel and then responds to HTTP Requests
+*
+*
+*/
+pub fn server(){
 
 	let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
@@ -50,7 +61,6 @@ pub fn listen_to_msg(){
 	for stream in listener.incoming() {
 	    match stream {
 	        Ok(mut stream) => {
-	        	println!("connected!");
 	        	let mut buffer = [0; 512];
 	        	let buff = stream.read(&mut buffer).unwrap();
 	        	let request = std::str::from_utf8(&buffer).unwrap();
@@ -59,11 +69,8 @@ pub fn listen_to_msg(){
 	        	let req_components = request.split(" ");
 	        	let mut split_req = vec![];
 	        	for comp in req_components{
-	        		println!("comp : {}", comp);
 	        		split_req.push(comp);
 	        	}
-	        	println!("{:?}", split_req);
-	        	println!("{} : {} ", split_req[0], split_req[1]);
 	        	respond(stream, HTTP_REQ::new(split_req[0].to_owned(), (split_req[1].to_owned())));	
 	        }
 	        Err(e) => { println!("Failure") }
@@ -80,6 +87,5 @@ pub fn listen_to_msg(){
 
 
 fn main() {
-    println!("Hello, world!");
-    listen_to_msg()
+    server()
 }
