f03cb469241c8aafe0cffa3738ae36a748c82755
JohnBCasey <caseyjohnb@gmail.com>
2017-08-09 23:45:43 -0400

    added example of how to deploy a server with binding with main method

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,17 @@ mod file_responder;
 mod server_instance;
 
 use server_instance::ServerInstance;
-
+use file_responder::specific_file_finder;
 
 
 fn main() {
-    let mut instance = ServerInstance::new("127.0.0.1:8080".to_owned()).run_server();
+
+    let mut si =  ServerInstance::new("127.0.0.1:8080".to_owned());
+
+    fn helloWorld(http_rq: http_req::HTTPRequest)->router::HttpResponse{
+            specific_file_finder("index.html".to_owned())
+    }
+
+    si.register_get_route("/helloWorld".to_owned(), helloWorld);
+    si.run_server()
 }
